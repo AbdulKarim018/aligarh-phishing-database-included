b33f95c75cc0cdedbecb175fee67665d72965512
AbdulKarim018 <lackhorse.ak@gmail.com>
2023-04-21 01:22:06 +0500

    endgame

--- database/index.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect('mongodb+srv://lackhorseak:lackhorseak@cluster0.qxrxovq.mongodb.net/aligarh').then(() => console.log("Connected To CloudDB!")).catch(err => console.log(err));
